2ca9122987e5dfd78795efcc0a5cc146b03007df
Ray Aeron <rayaeron95@hotmail.com>
2021-05-20 13:11:04 +0300

    Добавьте файлы проекта.

---
+++ APIska/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace APIska
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void r(object sender, RoutedEventArgs e)
+        {
+            string url = "https://evilinsult.com/generate_insult.php?lang=ru&type=json";
+            string response;
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+            InJson valueJSON = JsonConvert.DeserializeObject<InJson>(response);
+            string jopa = valueJSON.insult;
+            ListAPI.Text += "Оскорбление: " + jopa + "\r\n";
+        }
+    }
+}
